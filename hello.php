7f59c6127a700c193d351c4a0e8c95abd6e7a924
Ike Odira <odiraike8@gmail.com>
2023-02-15 15:26:57 -0800

    Delete comment

--- hello.php
+++ hello.php
@@ -11,22 +11,6 @@
 //     echo $index . ' - ' . $article, ". ";
 // }
 
-// $firstNumber = 20;
-// $secondNumber = 13;
-// $sum = $firstNumber + $secondNumber;
-
-// echo "The sum is " . $sum;
-
-// $ages = [18, 19, 55, 78, 12, 45];
-
-// foreach($ages as $age){
-//     if($age>18){
-//         echo  $age . " " . " You are eligible to vote ";
-//     }else{
-//         echo $age . " " . " You are not eligible ";
-//     }
-// }
-
 
 for($i = 1; $i <= 10; $i++){
     echo $i . ", ";
